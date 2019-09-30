ccd08e406ea4807ca98e7a6505e9efb0b6a4953e
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-10-01 01:14:58 +0530

    some new ui's

--- Cyber Restaurent.py
+++ Cyber Restaurent.py
@@ -1,1 +1,51 @@
-print("Hello Friends")
+from PyQt5 import QtWidgets
+import sys
+
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(640, 360)
+        self.setWindowTitle('Cyber Restaurent')
+        # self.setWindowIcon(ic_insert_table)
+        self.logged_user = ''
+
+        self.bar_menu = self.menuBar()
+        self.menu_bar()
+
+        self.bar_status = self.statusBar()
+        self.status_bar()
+
+        self.start()
+
+    def menu_bar(self):
+
+        quit_action = QtWidgets.QAction('&Quit Session', self)
+        quit_action.setShortcut('Ctrl+Q')
+        quit_action.setStatusTip('Quit Session')
+        # quit_action.triggered.connect(self.login_wid)
+
+        file_menu = self.bar_menu.addMenu('&File')
+        file_menu.addAction(quit_action)
+
+    def status_bar(self):
+        self.bar_status.showMessage('Ready to Go....')
+
+    def start(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+
+    win = MyWindow()
+    win.show()
+
+    end = app.exec_()
+    if not end:
+        try:
+            win.myc.close()
+        except AttributeError:
+            pass
+
+        sys.exit(end)
---
+++ images/__init__.py
@@ -0,0 +1,12 @@
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QGuiApplication
+import sys
+temp = QGuiApplication(sys.argv)
+im_correct = QPixmap("images\\correct.ico").scaled(30, 30)
+im_enter = QPixmap("images\\enter.ico").scaled(30, 30)
+ic_insert_table = QIcon("images\\insert_table.ico")
+im_wrong = QPixmap("images\\wrong.ico").scaled(30, 30)
+im_loading = QPixmap('images\\hourglass.ico').scaled(30, 30)
+im_net_error = QPixmap('images\\exclamation5.ico').scaled(30, 30)
+# sys.exit(temp.exec_())
