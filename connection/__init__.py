d858ff4d73104359be07d7b774beeabecb945696
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-10-07 04:00:29 +0530

    Useless directory deleted

--- connection/__init__.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-import dns
-# myc = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=3000)
-myc = MongoClient('mongodb+srv://therealrahulsahu:rahulsahu1_@'
-                  'democluster-2u6fb.gcp.mongodb.net/test?retryWrites=true', serverSelectionTimeoutMS=5000)
