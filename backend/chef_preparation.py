4120247bda26f98bd088f0492d11e136c583bcfe
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-11-06 20:23:13 +0530

    minor Changes

--- backend/chef_preparation.py
+++ backend/chef_preparation.py
@@ -8,14 +8,7 @@ def run_main_chef_preparation(curr_wid, MW):
     request_widget_list = []
     preparation_widget_list = []
 
-    def clear_layout(layout):
-        if layout is not None:
-            while layout.count():
-                child = layout.takeAt(0)
-                if child.widget() is not None:
-                    child.widget().deleteLater()
-                elif child.layout() is not None:
-                    clear_layout(child.layout())
+    from .common_functions import clear_layout
 
     class ThreadTakeOrder(QThread):
         signal = pyqtSignal('PyQt_PyObject')
