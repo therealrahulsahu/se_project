d882063b3cc044f5e63c649e31e16d3860d3b4b6
therealrahulsahu <43601158+therealrahulsahu@users.noreply.github.com>
2019-12-12 10:28:06 +0530

    Excess Removed

--- backend/chef_code.py
+++ backend/chef_code.py
@@ -5,11 +5,3 @@ class RunMain:
 
         from .chef_preparation import RunMainChefPreparation
         code_chef_preparation = RunMainChefPreparation(curr_wid, MW)
-
-
-def run_main(curr_wid, MW):
-    from .chef_change_food_ava import run_main_change_food_ava
-    run_main_change_food_ava(curr_wid, MW)
-
-    from .chef_preparation import run_main_chef_preparation
-    run_main_chef_preparation(curr_wid, MW)
--- backend/customer_code.py
+++ backend/customer_code.py
@@ -6,12 +6,3 @@ class RunMain:
         code_order_now = RunMainOrderNow(curr_wid, MW)
         code_status = RunMainStatus(curr_wid, MW)
         code_bill = RunMainBill(curr_wid, MW)
-
-
-def run_main(curr_wid, MW):
-    from .customer_order_now import run_main_order_now
-    from .customer_status import run_main_status
-    from .customer_bill import run_main_bill
-    run_main_order_now(curr_wid, MW)
-    run_main_status(curr_wid, MW)
-    run_main_bill(curr_wid, MW)
